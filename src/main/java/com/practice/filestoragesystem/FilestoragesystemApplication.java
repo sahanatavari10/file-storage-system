119adb11aaa3b1e29d86a9cd4eb77f69bcb086e3
Sahana Tavari <sahana.jagadeeshtavari@externos-uy.cencosud.com>
2025-05-11 13:07:54 +0530

    first commit

---
+++ src/main/java/com/practice/filestoragesystem/FilestoragesystemApplication.java
@@ -0,0 +1,13 @@
+package com.practice.filestoragesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilestoragesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilestoragesystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/filestoragesystem/FilestoragesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.filestoragesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestoragesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
